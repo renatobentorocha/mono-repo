d3e0445c09d75dd11984c98d86f08af517e3daa1
Renato Rocha <rbrocha@rd.com.br>
2024-12-26 11:58:35 -0300

    Add react-native-svg

--- packages/package1/components/Home/index.tsx
+++ packages/package1/components/Home/index.tsx
@@ -1,8 +1,10 @@
 import React from "react"
 import { Carousel } from "@example/package3"
+import { SvgExample } from "@example/package2"
 
 export const Home = () => {
   return (<>
     <Carousel />
+    <SvgExample />
   </>)
 }
